5c86520bcce3f18a6f3bbd06df1a65b9703bfb4f
Thomas Jachmann <self@thomasjachmann.com>
2013-06-19 23:38:40 +0200

    Put more calculation logic into the model

--- src/javascript/controllers/interest_ctrl.js
+++ src/javascript/controllers/interest_ctrl.js
@@ -8,7 +8,6 @@ function InterestCtrl($scope, $timeout, dataService) {
 
   $scope.select = function(index) {
     $scope.inputs = $scope.allInputs[index]
-    $scope.outputs = new Outputs();
     $scope.calculate();
   }
 
@@ -20,19 +19,12 @@ function InterestCtrl($scope, $timeout, dataService) {
   $scope.calculate = function() {
     $timeout.cancel(calculationTimer);
     $scope.save();
-    var inputs   = $scope.inputs,
-        outputs  = $scope.outputs;
-    outputs.brokerageCost  = inputs.price * inputs.brokerage / 100.0;
-    outputs.taxCost        = inputs.price * inputs.tax / 100.0;
-    outputs.notaryCost     = inputs.price * inputs.notary / 100.0;
-    outputs.cost           = inputs.price + outputs.brokerageCost + outputs.taxCost + outputs.notaryCost + inputs.addOns;
-    outputs.credit         = outputs.cost - inputs.capital;
-    outputs.payment        = Math.round(outputs.credit * (inputs.interestRate + inputs.amortizationRate) / 12) / 100.0;
+    var inputs   = $scope.inputs;
     $scope.months = null;
     $scope.processing = {
       months: [],
       month: null,
-      balance: outputs.credit,
+      balance: inputs.credit(),
     };
     calculationTimer = $timeout(processNextMonth, 100);
   };
@@ -41,7 +33,7 @@ function InterestCtrl($scope, $timeout, dataService) {
     times = times || 0;
     var data = $scope.processing;
     if (data.balance > 0) {
-      var month = new Month(data.balance, $scope.outputs.payment, $scope.inputs.interestRate / 100.0);
+      var month = new Month(data.balance, $scope.inputs.payment(), $scope.inputs.interestRate / 100.0);
       data.balance = month.remainder;
       data.months.push(month);
       if (times < 6) {
@@ -50,11 +42,11 @@ function InterestCtrl($scope, $timeout, dataService) {
         calculationTimer = $timeout(processNextMonth, 0);
       }
     } else {
-      $scope.outputs.totalInterest = 0;
-      $scope.outputs.totalPayments = 0;
+      $scope.inputs.totalInterest = 0;
+      $scope.inputs.totalPayments = 0;
       for (var month in data.months) {
-        $scope.outputs.totalInterest += data.months[month].interest;
-        $scope.outputs.totalPayments += data.months[month].payment;
+        $scope.inputs.totalInterest += data.months[month].interest;
+        $scope.inputs.totalPayments += data.months[month].payment;
       };
       $scope.months = data.months;
       $scope.processing = null;
